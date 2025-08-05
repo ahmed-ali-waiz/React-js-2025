570d917872dd4c32f39938f9db1d0c571618cc35
Ahmad Ali <103488484+ahmed-ali-waiz@users.noreply.github.com>
2025-08-05 19:11:10 +0000

    Password Generator

--- 05Passwordgenerator/src/App.jsx
+++ 05Passwordgenerator/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useCallback } from 'react'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberallowed, setnumberallowed] = useState(false);
+  const [charallowed, setcharallowed] = useState(false);
+  const [password , setpassword] = useState("")
+
+  const passwordGenerator = useCallback(()=>{
+    
+  } , [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button className='text-amber-300 bg-blue-500 rounded-full px-4 py-2'>Click me</button>
-    </>
+   <div className="h-screen bg-[#212121] text-white flex items-center justify-center">
+    <h1 className='text-4xl text-center '>
+    I am Ahmad
+    </h1>
+   </div>
   )
 }
 
