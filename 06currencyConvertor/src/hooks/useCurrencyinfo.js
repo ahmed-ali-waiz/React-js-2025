bfbeb5fc3958cf62583ac30f1beb6ecd306ed115
Ahmad Ali <103488484+ahmed-ali-waiz@users.noreply.github.com>
2025-08-06 22:37:34 +0000

    Custom hooks

---
+++ 06currencyConvertor/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,16 @@
+import { useEffect,useState } from "react";
+
+function useCurrencyinfo(currency){
+
+    const [data,setdata] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res) =>setdata([currency]) )
+        console.log(data);
+        
+    },[currency])
+
+    return data
+}
+export default useCurrencyinfo
