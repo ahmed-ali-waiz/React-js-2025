67d83074548087eb3017fa636a970ada1755e036
Ahmad Ali <103488484+ahmed-ali-waiz@users.noreply.github.com>
2025-08-06 22:00:19 +0000

    Currency Convertor

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+  <div className=''>
+    <h1 className='text-2xl text-amber-800'>
+      Ahmad Ali
+    </h1>
+  </div>
+  )
+}
+
+export default App
